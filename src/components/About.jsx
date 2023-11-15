410d8056cc12583f4b05404e3f436c7ef8b6c311
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2023-11-15 22:13:25 +0700

    commit first

---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import Photo from "../assets/photo-profile.png";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="pb-8 pl-4 sm:text-center">
+            <p className="inline text-4xl font-bold border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4 mt-4">
+          <div className="text-4xl font-bold sm:text-right place-self-center">
+            <img src={Photo} alt="Ihsan Tri Marseno" className="rounded-full"/>
+          </div>
+          <div className="flex items-center tracking-widest lg:text-xl">
+            <p>
+              {`I'm Ihsan Tri Marseno, a 20-year-old with a passion for carving my
+              career path in front-end development and UI/UX. While my primary
+              focus is on front-end, I'm intrigued by the dynamic world of
+              UI/UX. During my leisure time, I dedicate myself to honing my
+              skills in these domains, constantly seeking to refine and expand
+              my knowledge`}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-2xl text-[#FF5757] font-semibold">Hello, </p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Ihsan Tri Marseno
+          <span className="text-[#FF5757] text-2xl"> {`'s here`}</span>
+        </h1>
+        <h2 className="text-4xl sm:text-4xl font-bold text-[#8892b0]">
+          {`Front-end and UI/UX Enthusiast.`}
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I am a Computer Science student at UPN Veteran Jakarta, with a current
+          focus on front-end web development. Below is a showcase of my recent
+          work
+        </p>
+        <div>
+          <Link to="work" smooth={true} duration={1000}>
+            <button className="flex items-center px-6 py-3 my-2 text-white border-2 group hover:bg-[#FF5757] hover:border-[#FF5757]">
+              View Work
+              <span className="duration-300 group-hover:rotate-90">
+                <HiArrowNarrowRight className="ml-3 " />
+              </span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
