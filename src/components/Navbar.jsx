cd25ae5fc006eb202ed05398d2d5ffd27d32827b
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2023-11-16 12:07:49 +0700

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -76,8 +76,8 @@ const Navbar = () => {
         </li>
         <li className="py-6 text-4xl">
           {" "}
-          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
-            Work
+          <Link onClick={handleClick} to="projects" smooth={true} duration={500}>
+            Projects
           </Link>
         </li>
         <li className="py-6 text-4xl">
