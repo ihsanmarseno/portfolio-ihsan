50f7f7c25e214845f55acbcb431ffd0decce0520
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2023-11-18 11:52:56 +0700

    add footer and app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-center w-full h-[80px] bg-[#0a192f]'>
+        <p className='text-white '>Made with ❤️ by Ihsan Tri Marseno | 2023</p>
+    </div>
+  )
+}
+
+export default Footer
