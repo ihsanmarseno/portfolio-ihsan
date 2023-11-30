c11bedbee20aa26970db69a7bd9222a86c02394d
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2023-11-30 21:22:08 +0700

    add flex about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import Photo from "../assets/photo-profile.png";
 import Next from "../assets/next.png";
 import React from "../assets/react.png";
 import Tailwind from "../assets/tailwind.png";
-import Typescript from "../assets/typescript.png"
+import Typescript from "../assets/typescript.png";
 
 const About = () => {
   useEffect(() => {
@@ -36,26 +36,28 @@ const About = () => {
             />
           </div>
           <div
-            className="items-center text-sm tracking-wide text-gray-300 md:text-lg"
+            className="flex items-center text-sm tracking-wide text-gray-300 md:text-lg "
             data-aos="fade-right"
             data-aos-delay="400"
           >
-            <p>
-              {`I'm Ihsan Tri Marseno, a 20-year-old with a passion for carving my
+            <div className="flex-col items-center">
+              <p>
+                {`I'm Ihsan Tri Marseno, a 20-year-old with a passion for carving my
               career path in front-end development and UI/UX. While my primary
               focus is on front-end, I'm intrigued by the dynamic world of
               UI/UX. During my leisure time, I dedicate myself to honing my
               skills in these domains, constantly seeking to refine and expand
               my knowledge.`}
-            </p>
-            <p className="mt-4 md:mt-8 text-base md:text-xl text-[#2ea4ff] font-semibold tracking-wider">
-              Current Favorite Tech:{" "}
-            </p>
-            <div className="flex items-center w-8 h-8 gap-4 md:h-12 md:w-12">
-              <img src={Next} alt="Next Js" className="mt-4 " />
-              <img src={Typescript} alt="Typescript" className="mt-4 " />
-              <img src={React} alt="React Js" className="mt-4 " />
-              <img src={Tailwind} alt="Tailwind" className="mt-4 " />
+              </p>
+              <p className="mt-4 md:mt-8 text-base md:text-xl text-[#2ea4ff] font-semibold tracking-wider">
+                Current Favorite Tech:{" "}
+              </p>
+              <div className="flex items-center w-8 h-8 gap-4 md:h-12 md:w-12">
+                <img src={Next} alt="Next Js" className="mt-4 " />
+                <img src={Typescript} alt="Typescript" className="mt-4 " />
+                <img src={React} alt="React Js" className="mt-4 " />
+                <img src={Tailwind} alt="Tailwind" className="mt-4 " />
+              </div>
             </div>
           </div>
         </div>
