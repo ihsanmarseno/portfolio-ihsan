0882cc945e036ab80eacbbfc4dd41356594a2d62
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2023-12-28 10:31:43 +0700

    add link on email and skills

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
         <form method='POST' action="https://formspree.io/f/xpzgelow" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='inline md:text-4xl text-3xl font-bold text-white border-b-4 border-[#FF5757]'>Contact</p>
-                <p className='py-4 text-sm text-gray-300 md:text-lg'>{`// Submit the form below or email - ihsanmarseno09@gmail.com`}</p>
+                <p className='py-4 text-sm text-gray-300 md:text-lg'>{`// Submit the form below or email - `} <span className="underline"><a className='text-[#EAD196]' href="mailto:ihsanmarseno09@gmail.com">ihsanmarseno09@gmail.com</a></span></p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' required/>
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' required/>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,12 +3,16 @@ import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import Typescript from '../assets/typescript.png';
+import Java from '../assets/java.webp';
+import MySQL from '../assets/mysql.png';
+import SQLite from '../assets/sqlite.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
 import Next from '../assets/next.png';
 import Bootstrap from '../assets/bootstrap.svg';
+import daisyUI from '../assets/daisyui.png';
 import Figma from '../assets/figma.png'; 
 import Canva from '../assets/canva.png';
 import 'aos/dist/aos.css';
@@ -25,14 +29,18 @@ const Skills = () => {
     { id: 2, image: CSS, alt: 'CSS icon', label: 'CSS' },
     { id: 3, image: JavaScript, alt: 'JavaScript icon', label: 'JavaScript' },
     { id: 4, image: Typescript, alt: 'Typescript icon', label: 'Typescript' },
-    { id: 5, image: ReactImg, alt: 'React icon', label: 'React Js' },
-    { id: 6, image: GitHub, alt: 'GitHub icon', label: 'GitHub' },
-    { id: 7, image: Node, alt: 'Node icon', label: 'Node Js' },
-    { id: 8, image: Next, alt: 'Next icon', label: 'Next Js' },
-    { id: 9, image: Tailwind, alt: 'Tailwind icon', label: 'Tailwind CSS' },
-    { id: 10, image: Bootstrap, alt: 'Bootstrap icon', label: 'Bootstrap' },
-    { id: 11, image: Figma, alt: 'Figma icon', label: 'Figma' },
-    { id: 12, image: Canva, alt: 'Canva icon', label: 'Canva' },
+    { id: 5, image: Java, alt: 'Java icon', label: 'Java' },
+    { id: 6, image: MySQL, alt: 'MySQL icon', label: 'MySQL' },
+    { id: 7, image: SQLite, alt: 'SQLite icon', label: 'SQLite' },
+    { id: 8, image: ReactImg, alt: 'React icon', label: 'React Js' },
+    { id: 9, image: GitHub, alt: 'GitHub icon', label: 'GitHub' },
+    { id: 10, image: Node, alt: 'Node icon', label: 'Node Js' },
+    { id: 11, image: Next, alt: 'Next icon', label: 'Next Js' },
+    { id: 12, image: Tailwind, alt: 'Tailwind icon', label: 'Tailwind CSS' },
+    { id: 13, image: Bootstrap, alt: 'Bootstrap icon', label: 'Bootstrap' },
+    { id: 14, image: daisyUI, alt: 'daisyUI icon', label: 'daisyUI' },
+    { id: 15, image: Figma, alt: 'Figma icon', label: 'Figma' },
+    { id: 16, image: Canva, alt: 'Canva icon', label: 'Canva' },
   ];
 
 
